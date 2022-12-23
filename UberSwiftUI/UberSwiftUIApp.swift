b2a3a87eb2b25ead7c9c813a71a5a3c568541281
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-23 14:59:29 +0100

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Philipp Sanktjohanser on 23.12.22.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
